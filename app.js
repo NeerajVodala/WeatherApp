41e57b75bfaa79e23b16610c3dd99b5801645ee0
NeerajVodala <vodalaneeraj@gmail.com>
2021-10-14 21:41:05 +0530

    not done

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const locationName = document.querySelector(".location-name");
-const temperatureValue = document.querySelector(".temperature-value");
-const temperatureIcon = document.querySelector(".temperature-icon");
-const temperatureDescription = document.querySelector(".temperature-description");
+
+
+
 
 let apikey = "8153c938a33762c3016a056bfce61087";
-let city = "hyderabad";
+let city = "nairobi";
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apikey).then(response => response.json()).then(json => {
-    console.log(json, json.name, json.sys.country, json.main.temp, json.main.humidity, json.weather[0].description)
-
-    locationName.innerText = json.name.toUpperCase() + ", " + json.sys.country;
-    temperatureValue.innerText = json.main.temp + "° C";
-    temperatureDescription.innerText =  json.weather[0].description.toUpperCase();
+fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apikey)
+.then(response => response.json())
+.then(data => {
+    console.log(data, data.weather[0].icon);
+    let iconId = data.weather[0].icon;
+    fetch("https://openweathermap.org/img/wn/" + iconId + "@2x.png").then(document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + iconId + "@2x.png")
 });
+
+
+
 
